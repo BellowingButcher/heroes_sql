e7c3f70ff0dd21fe9b53ecd76b2f35bb87d5b533
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-03 00:00:15 +0000

    trying to get my join statment to work
    - its inside of whateveryouwant

--- src/whateveryouwant.py
+++ src/whateveryouwant.py
@@ -1,1 +1,10 @@
-from database.connection import execute_query
+from database.connection import execute_query;
+
+def execute_query("SELECT * FROM users", params=None):
+
+SELECT heroes.id, relationships.relationship_type_id, relationship_type_id.name
+FROM heroes
+INNER JOIN relationships
+ON heroes.id = relationships.hero1_id
+INNER JOIN relationships
+ON relationship_type_id.id = relationships.relationship_type_id
