c9bfcdb3db00e3f1d6eff8323cbf3fe989ccd28b
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-03 16:14:53 +0000

    addsuper, learnabout, and updateaboutme works

--- src/learn_about_who.py
+++ src/learn_about_who.py
@@ -1,10 +1,9 @@
-from database.connection import execute_query
+# hero_table = execute_query(query)
 name = input('What hero are you inquiring about? ')
 action = """SELECT *
             FROM heroes WHERE name = %s"""
 
 result = execute_query(action, (name,)).fetchall()
-print(result)
 print('Hello my name is ' + result[0][1] + ' and ' + result[0][2])
 print('My story? Sure Ill tell you my story. Listen up!')
 print(result[0][3])
---
+++ src/update_about_me.py
@@ -0,0 +1,8 @@
+from database.connection import execute_query
+
+name = input('Whats your name? ')
+new = input('Whats new about you? ')
+action = """UPDATE heroes
+            SET about_me = %s
+            WHERE name=%s"""
+execute_query(action,(new, name))
