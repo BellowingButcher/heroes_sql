5cf8fd340e02ec1d119fcc86cae41ee5a6204939
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-04 01:25:56 +0000

    ui update

--- src/run_game.py
+++ src/run_game.py
@@ -8,7 +8,7 @@ from make_a_relationship import make_a_relation
 from see_all import see_all
 
 def run_game():
-    choice = input('Thank you for accessing the Super Hero database. Use one of the following keywords to interact with our database: CREATE, UPDATE, DISPATCH, RELATIONS, LEARN, CONNECTIONS, LIST, HELP, QUIT: ').upper()
+    choice = input('Thank you for accessing the Super Hero database. Use one of the following keywords to interact with our database: CREATE, UPDATE, DISPATCH, RELATIONS, LEARN, CONNECTIONS, LIST, HELP, QUIT.\nHint - type HELP if it is your first time here: ').upper()
     if choice == 'HELP':
         print('CREATE - Add a new super hero to the database\nUPDATE - Change the about me of a specific hero\n DISPATCH - Remove hero from the database\nRELATIONS - Display a list of friends and enemies of an entered hero\nLEARN - Learn about a specific hero\nCONNECTIONS - Make a new friend, or enemy\nLIST - See a list of all the heroes in the database\nHELP - See this prompt\nQUIT - you wont')
         onward()
--- src/update_about_me.py
+++ src/update_about_me.py
@@ -1,7 +1,12 @@
 from database.connection import execute_query
 def update():
-    name = input('Whats your name? ')
-    new = input('Whats new about you? ')
+    heroes = """SELECT id, name FROM heroes"""
+    heroes_result = execute_query(heroes).fetchall()
+    for x in heroes_result:
+        print("-"+str(x[1]))
+
+    name = input("Who's about me do you want to update? ")
+    new = input('Whats new about them? ')
     action = """UPDATE heroes
                 SET about_me = %s
                 WHERE name=%s"""
